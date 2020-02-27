e749271fb49bf6386019737953f484b849970051
intromisan <42156539+intromisan@users.noreply.github.com>
2020-02-27 20:37:40 +0100

    mobile version
    Added features:
      auto-download CV;
      skills box on click;
      auto-call;
      other minor changes;

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const menu = document.querySelector(".menu");
 const overlay = document.querySelector(".overlay");
+const skills = document.querySelector('.skills');
+const skillsBox = document.querySelector(".skills-box");
 
-menu.addEventListener('click', function() {
+menu.addEventListener('click', () => {
     menu.classList.toggle('active');
     overlay.classList.toggle('open');
+});
+
+skills.addEventListener('click', () => {
+    skillsBox.classList.toggle('open');
 });
